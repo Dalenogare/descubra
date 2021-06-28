8a47db38a7d35c41dddeabf166eb10b56dc6613f
dalenogare98 <dalenogare98@gmail.com>
2021-06-28 01:24:07 -0300

    integrando banco de dados

---
+++ application/backend/src/config/database.js
@@ -0,0 +1,11 @@
+module.exports = {
+    dialect: 'postgres',
+    host: 'localhost',
+    username: 'postgres',
+    password: 'Dalenogare98',
+    database: 'project',
+    define: {
+        timestamps: true,
+        underscored: true,
+    },
+};
--- application/frontend/src/components/user/UserCrud.jsx
+++ application/frontend/src/components/user/UserCrud.jsx
@@ -8,7 +8,7 @@ const headerProps = {
     subtitle: 'Cadastro de usuários: Incluir, Listar, Alterar e Excluir'
 }
 
-const baseUrl = 'http://localhost:3001/users'
+const baseUrl = '/users/'
 const initialState = {
     user: { name: '', email: '' },
     list: []
@@ -18,11 +18,11 @@ export default class UserCrud extends Component {
 
     state = { ...initialState }
 
-    componentWillMount() {
-        axios(baseUrl).then(resp => {
-            this.setState({ list: resp.data })
-        })
-    }
+    // componentWillMount() {
+    //     axios(baseUrl).then(resp => {
+    //         this.setState({ list: resp.data })
+    //     })
+    // }
 
     clear() {
         this.setState({ user: initialState.user })
@@ -31,19 +31,22 @@ export default class UserCrud extends Component {
     save() {
         const user = this.state.user
         const method = user.id ? 'put' : 'post'
+        const jsonObject = JSON.stringify(user)
         const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
         axios[method](url, user)
             .then(resp => {
-                const list = this.getUpdatedList(resp.data)
-                this.setState({ user: initialState.user, list })
+                console.log(resp)
+                //const list = this.getUpdatedList(resp.data)
+                //this.setState({ user: initialState.user, list })
             })
+        this.clear()
     }
 
-    getUpdatedList(user, add = true) {
-        const list = this.state.list.filter(u => u.id !== user.id)
-        if(add) list.unshift(user)
-        return list
-    }
+    // getUpdatedList(user, add = true) {
+    //     const list = this.state.list.filter(u => u.id !== user.id)
+    //     if(add) list.unshift(user)
+    //     return list
+    // }
 
     updateField(event) {
         const user = { ...this.state.user }
@@ -107,51 +110,51 @@ export default class UserCrud extends Component {
         })
     }
 
-    renderTable() {
-        return (
-            <table className="table mt-4">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Nome</th>
-                        <th>Email</th>
-                        <th>Ações</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.renderRows()}
-                </tbody>
-            </table>
-        )
-    }
+    // renderTable() {
+    //     return (
+    //         <table className="table mt-4">
+    //             <thead>
+    //                 <tr>
+    //                     <th>ID</th>
+    //                     <th>Nome</th>
+    //                     <th>Email</th>
+    //                     <th>Ações</th>
+    //                 </tr>
+    //             </thead>
+    //             <tbody>
+    //                 {this.renderRows()}
+    //             </tbody>
+    //         </table>
+    //     )
+    // }
 
-    renderRows() {
-        return this.state.list.map(user => {
-            return (
-                <tr key={user.id}>
-                    <td>{user.id}</td>
-                    <td>{user.name}</td>
-                    <td>{user.email}</td>
-                    <td>
-                        <button className="btn btn-warning"
-                        onClick={() => this.load(user)}>
-                            <i className="fa fa-pencil"></i>
-                        </button>
-                        <button className="btn btn-danger ml-2"
-                        onClick={() => this.remove(user)}>
-                            <i className="fa fa-trash"></i>
-                        </button>
-                    </td>
-                </tr>
-            )
-        })
-    }
+    // renderRows() {
+    //     return this.state.list.map(user => {
+    //         return (
+    //             <tr key={user.id}>
+    //                 <td>{user.id}</td>
+    //                 <td>{user.name}</td>
+    //                 <td>{user.email}</td>
+    //                 <td>
+    //                     <button className="btn btn-warning"
+    //                     onClick={() => this.load(user)}>
+    //                         <i className="fa fa-pencil"></i>
+    //                     </button>
+    //                     <button className="btn btn-danger ml-2"
+    //                     onClick={() => this.remove(user)}>
+    //                         <i className="fa fa-trash"></i>
+    //                     </button>
+    //                 </td>
+    //             </tr>
+    //         )
+    //     })
+    // }
 
     render() {
         return (
             <Main {...headerProps}>
                 {this.renderForm()}
-                {this.renderTable()}
+                {/* {this.renderTable()} */}
             </Main>
         )
     }
