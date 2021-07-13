90ee87ccb42db74f6a5b27f681717aa80ae54af0
dalenogare98 <dalenogare98@gmail.com>
2021-07-13 08:07:26 -0300

    autentitação

--- application/frontend/src/components/login/Login.jsx
+++ application/frontend/src/components/login/Login.jsx
@@ -1,19 +1,24 @@
 import React, { useState } from 'react';
 import './Login.css';
 import PropTypes from 'prop-types';
+import axios from 'axios'
 
 export default function Login({ setToken }) {
 
     async function loginUser(credentials) {
-        return fetch('http://localhost:3333/login', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify(credentials)
-        })
-          .then(data => data.json())
-       }
+        //     return fetch('http://localhost:3333/login', {
+        //       method: 'POST',
+        //       headers: {
+        //         'Content-Type': 'application/json'
+        //       },
+        //       body: JSON.stringify(credentials)
+        //     })
+        //       .then(data => data.json())
+        //    }
+        const login = await axios.post('http://localhost:3333/login', credentials)
+            .then(resp => resp.data)
+        return login
+    }
 
     const [username, setUserName] = useState();
     const [password, setPassword] = useState();
@@ -21,9 +26,10 @@ export default function Login({ setToken }) {
     const handleSubmit = async e => {
         e.preventDefault();
         const token = await loginUser({
-          username,
-          password
+            username,
+            password
         });
+        console.log(token)
         setToken(token);
     }
 
--- application/frontend/src/main/App.jsx
+++ application/frontend/src/main/App.jsx
@@ -1,7 +1,7 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 import './App.css'
-import React, { useState } from 'react'
+import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
 
 import Routes from './Routes'
@@ -9,9 +9,12 @@ import Logo from '../components/template/Logo'
 import Nav from '../components/template/Nav'
 import Footer from '../components/template/Footer'
 import Login from '../components/login/Login';
+import useToken from './useToken';
+
 
 function App() {
-    const [token, setToken] = useState();
+    
+    const { token, setToken } = useToken();
 
     if (!token) {
         return (
