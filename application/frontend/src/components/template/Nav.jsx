6a0a1ff320576eb2e476f4e93382d5637c71c59e
Pedro Silveira Dalenogare <dalenogare98@gmail.com>
2021-07-16 15:34:22 -0300

    logout button

--- application/frontend/src/components/template/Nav.jsx
+++ application/frontend/src/components/template/Nav.jsx
@@ -2,6 +2,11 @@ import './Nav.css'
 import NavItem from './NavItem.jsx'
 import React from 'react'
 
+function logOut() {
+    sessionStorage.removeItem('token')
+    window.location.reload()
+}
+
 export default props =>
     <aside className="menu-area">
         <nav className="menu">  
@@ -10,8 +15,8 @@ export default props =>
             <NavItem navigateTo="" faClass="money" name="Meu Restaurante"/>
             <NavItem navigateTo="/mail" faClass="envelope-o" name="Contato"/>
             <div className="footerButton">
-                <NavItem navigateTo="" faClass="sign-out" name="Sair"/>        
-                {/* <button type="button"className="fa fa-sign-out" onClick={e => console.log(props.token)}>Sair</button>         */}
+                {/* <NavItem navigateTo="/mail" faClass="sign-out" name="Sair" onClick={e => sessionStorage.removeItem('token')}/>         */}
+                <button id="footButton" navigateTo="/mail" type="button" onClick={e => logOut()}>Sair</button>        
             </div>      
         </nav>
     </aside>
